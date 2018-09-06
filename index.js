71c8ed7531ad1cf89ba654d8f5db9f63c26ed775
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-06 19:49:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const match =  drivers.filter(function (item) {return item.toUpperCase() === str
 
 function fuzzyMatch(drivers, string) {
 
-  const match =  drivers.filter(function (item) {return item.name.toUpperCase().includes(string.toUpperCase()) })
-    return match
+  const new =  drivers.filter(function (item) {return item.name.toUpperCase().includes(string.toUpperCase()) })
+    return new
 }
