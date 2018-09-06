e75c4ec2472800e5133a68045144f89f60ff4fdb
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-06 19:40:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
 function findMatching(drivers,string) {
-const match =  drivers.filter(function (item) {return item === string })
+const match =  drivers.filter(function (item) {return item.toUpperCase() === string.toUpperCase() })
   return match
 }
