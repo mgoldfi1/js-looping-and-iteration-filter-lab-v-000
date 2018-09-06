e44c562a04abd07e0a4d4a48176968806998474b
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-06 19:54:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function fuzzyMatch(drivers, string) {
   const newArray =  drivers.filter(function (item) {return item[0] === string[0] && item[1] === string[1]})
     return newArray
 }
+
+function matchName(drivers,string) {
+  const newArray =  drivers.filter(function (item) {return item.name === string})
+    return newArray
+}
